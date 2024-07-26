a424f12f7fd2a2fe5b31f800c83c6db165484062
Kush <85061303+kush992@users.noreply.github.com>
2024-07-26 16:34:18 +0200

    added analytics and wrapped inside suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,31 @@
 import Application from '@/components/Applications';
-import Image from 'next/image';
-import { useSearchParams } from 'next/navigation';
+import { Analytics } from '@vercel/analytics/next';
+import { Suspense } from 'react';
 
 export default function Home() {
 	return (
-		<main className='flex min-h-screen flex-col gap-8 max-w-8xl mx-auto p-4 md:p-16'>
-			<div className=''>
-				<h1 className='text-lg'>Hello, I'm Kush</h1>
-				<br />
-				<p className='text-xs text-gray-700'>
-					This app is built for managing and keeping track of my applied jobs. If you are reading this, then it means that this application
-					is not yet completely built and it is out because of my current job search.{' '}
-					<b className='text-gray-900'>Data is unavailable to you</b>
-				</p>
-				<br />
-				<p className='text-xs text-gray-700'>
-					If you are interested in this app, feel free to bookmark this url and visit it in near future.
-				</p>
-				<p className='text-xs text-gray-700'>
-					To connect with me, head over to <a href='https://kushbhalodi.com/'>kushbhalodi.com</a>
-				</p>
-			</div>
+		<Suspense fallback={<div>Loading...</div>}>
+			<main className='flex min-h-screen flex-col gap-8 max-w-8xl mx-auto p-4 md:p-16'>
+				<div className=''>
+					<h1 className='text-lg'>Hello, {"I'm"} Kush</h1>
+					<br />
+					<p className='text-xs text-gray-700'>
+						This app is built for managing and keeping track of my applied jobs. If you are reading this, then it means that this
+						application is not yet completely built and it is out because of my current job search.{' '}
+						<b className='text-gray-900'>Data is unavailable to you</b>
+					</p>
+					<br />
+					<p className='text-xs text-gray-700'>
+						If you are interested in this app, feel free to bookmark this url and visit it in near future.
+					</p>
+					<p className='text-xs text-gray-700'>
+						To connect with me, head over to <a href='https://kushbhalodi.com/'>kushbhalodi.com</a>
+					</p>
+				</div>
 
-			<Application />
-		</main>
+				<Analytics />
+				<Application />
+			</main>
+		</Suspense>
 	);
 }
