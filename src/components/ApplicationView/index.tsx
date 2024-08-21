5517eff3bb68f885aee705515bc94563fd1f63df
Kush <85061303+kush992@users.noreply.github.com>
2024-08-21 03:23:56 +0200

    added missing loading screen and error message

--- src/components/ApplicationView/index.tsx
+++ src/components/ApplicationView/index.tsx
@@ -6,6 +6,7 @@ import SubHeader from '../SubHeader';
 import Link from 'next/link';
 import { appRoutes } from '@/utils/constants';
 import { formatDate } from '@/utils/date';
+import Loader from '../Loader';
 
 type Props = {
 	documentId: string;
@@ -17,6 +18,7 @@ const ApplicationView = ({ documentId }: Props) => {
 
 	useEffect(() => {
 		async function fetchApplicationData() {
+			setIsFetching(true);
 			try {
 				const response: JobApplicationData = await database.getDocument(
 					appwriteDatabaseConfig.applicationDatabase,
@@ -26,6 +28,8 @@ const ApplicationView = ({ documentId }: Props) => {
 				setApplicationData(response);
 			} catch (error) {
 				console.error(error);
+			} finally {
+				setIsFetching(false);
 			}
 		}
 		fetchApplicationData();
@@ -33,37 +37,46 @@ const ApplicationView = ({ documentId }: Props) => {
 
 	return (
 		<div className='flex flex-col gap-6'>
-			<div className='flex items-center justify-between'>
-				<div>
-					<SubHeader previousPageTitle='Applications' href={appRoutes.applicationPage} />
-					<h1 className='text-xl font-semibold !m-0'>
-						{applicationData.jobTitle} at {applicationData.companyName}
-					</h1>
-				</div>
-				<Link href={`${appRoutes.updateApplicationPage}/${documentId}`} className='underline'>
-					Edit
-				</Link>
-			</div>
+			{isFetching && <Loader />}
+			{!isFetching && !applicationData.$id && <p>Application data not found</p>}
+			{!isFetching && (
+				<>
+					<div className='flex items-center justify-between'>
+						<div>
+							<SubHeader previousPageTitle='Applications' href={appRoutes.applicationPage} />
+							<h1 className='text-xl font-semibold !m-0'>
+								{applicationData.jobTitle} at {applicationData.companyName}
+							</h1>
+						</div>
+						<Link href={`${appRoutes.updateApplicationPage}/${documentId}`} className='underline'>
+							Edit
+						</Link>
+					</div>
 
-			<div className='bg-slate-100 p-4 rounded-lg'>
-				<div>
-					<p className='text-sm text-gray-500'>
-						Applied on: <strong>{new Date(applicationData.$createdAt).toLocaleDateString()}</strong>
-					</p>
-					<p className='text-sm text-gray-500'>Status: {applicationData.applicationStatus ?? '-'}</p>
-					<p className='text-sm text-gray-500'>
-						Salary: {applicationData.salary} {applicationData?.salaryCurrency?.toLowerCase()}
-						{applicationData?.salaryType?.toLocaleLowerCase()}
-					</p>
-					{applicationData?.interviewDate && (
-						<p className='text-sm text-gray-500'>
-							Interview Date: <b>{formatDate(applicationData.interviewDate)}</b>
-						</p>
-					)}
-				</div>
+					<div className='bg-slate-100 p-4 rounded-lg'>
+						<div>
+							<p className='text-sm text-gray-500'>
+								Applied on: <strong>{formatDate(applicationData.$createdAt)}</strong>
+							</p>
+							<p className='text-sm text-gray-500'>Status: {applicationData.applicationStatus ?? '-'}</p>
+							<p className='text-sm text-gray-500'>
+								Salary: {applicationData.salary} {applicationData?.salaryCurrency?.toLowerCase()}
+								{applicationData?.salaryType?.toLocaleLowerCase()}
+							</p>
+							{applicationData?.interviewDate && (
+								<p className='text-sm text-gray-500'>
+									Interview Date: <b>{formatDate(applicationData.interviewDate)}</b>
+								</p>
+							)}
+						</div>
 
-				<div className='bg-grey rounded-lg reset-styles !py-3' dangerouslySetInnerHTML={{ __html: applicationData?.jobDescription }} />
-			</div>
+						<div
+							className='bg-grey rounded-lg reset-styles !py-3'
+							dangerouslySetInnerHTML={{ __html: applicationData?.jobDescription }}
+						/>
+					</div>
+				</>
+			)}
 		</div>
 	);
 };
