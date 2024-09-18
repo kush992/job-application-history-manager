9d1439026e774afb2e9388ed9fd939bf776b0ec8
Kush <85061303+kush992@users.noreply.github.com>
2024-09-18 18:53:17 +0200

    added loader in suspense

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,13 +3,14 @@ import { Input } from 'antd';
 import { getLoggedInUser, signUpWithEmail } from '@/lib/server/appwrite';
 import { redirect } from 'next/navigation';
 import { EyeOutlined, MailOutlined, UserOutlined } from '@ant-design/icons';
+import Loader from '@/components/Loader';
 
 const SignupPage: React.FC = async () => {
 	const user = await getLoggedInUser();
 	if (user) redirect('/applications');
 
 	return (
-		<Suspense fallback='loading....'>
+		<Suspense fallback={<Loader />}>
 			<div className='font-[sans-serif] max-w-4xl flex items-center mx-auto md:h-screen p-4'>
 				<div className='grid md:grid-cols-3 items-center shadow-[0_2px_10px_-3px_rgba(62,107,209,0.3)] rounded-xl overflow-hidden bg-white'>
 					<div className='max-md:order-1 flex flex-col justify-center space-y-16 max-md:mt-16 min-h-full bg-gradient-radial from-gray-900 to-gray-700 lg:px-8 px-4 py-4'>
