6b05b1e01acf879b015852743ba1e343ceb74d58
Kush <85061303+kush992@users.noreply.github.com>
2024-10-24 18:30:09 +0200

    Feat: Added hiding mechanism for the "Currently on testing stage" message on header (#11)
    * added footer
    
    * added hiding functionality for testing message
    
    * updated implementation to check on the component mount event and toggle the message

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,30 +4,55 @@ import { appRoutes } from '@/utils/constants';
 import cn from 'classnames';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { PlusCircleFilled } from '@ant-design/icons';
 import { Models } from 'appwrite';
 // import { ThemeSwitcher } from '../ThemeSwitcher';
 import { Button } from '@/components/ui/button';
 import { UserMenu } from '../UserMenu';
-import { Info } from 'lucide-react';
+import { CircleX, Info, X } from 'lucide-react';
 
 type Props = {
 	user: Models.User<Models.Preferences> | null;
 };
 
 const Header: React.FC<Props> = ({ user }) => {
+	const [isHidden, setIsHidden] = useState<boolean>(true);
 	const pathname = usePathname();
 
 	const isActive = (route: string) => {
 		return pathname === route;
 	};
 
+	const hideTestingInfo = () => {
+		const isHidden = localStorage.getItem('isHidden');
+		if (isHidden === 'true') {
+			setIsHidden(true);
+		}
+
+		localStorage.setItem('isHidden', 'true');
+		setIsHidden(true);
+	};
+
+	useEffect(() => {
+		const isHidden = localStorage.getItem('isHidden');
+		if (isHidden === 'true') {
+			setIsHidden(true);
+		} else {
+			setIsHidden(false);
+		}
+	}, []);
+
 	return (
 		<>
-			<div className='bg-background text-muted-foreground text-xs py-2 text-center border-b flex gap-1 items-center justify-center'>
-				<Info className='w-5 h-5' /> Currently on testing stage
-			</div>
+			{!isHidden && (
+				<div className='bg-background text-muted-foreground text-xs py-2 text-center border-b flex gap-2 items-center justify-center'>
+					Currently on testing stage{' '}
+					<Button size='icon' variant='ghost' className='w-7 h-7' onClick={hideTestingInfo}>
+						<X className='cursor-pointer w-5 h-5' />
+					</Button>
+				</div>
+			)}
 			<header className='border-b border-muted z-50 sticky top-0 backdrop-blur duration-200 bg-background'>
 				<div className='max-w-6xl mx-auto flex justify-between items-center h-[50px] px-4'>
 					<div>
