6b84373969165cf102223f014732b70650576f30
Kush <85061303+kush992@users.noreply.github.com>
2024-11-04 13:25:51 +0100

    added about us page

---
+++ src/components/AboutUsPage/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { aboutUsContent } from './utility';
+
+const AboutUsPage: React.FC = () => {
+	return (
+		<div className="md:text-center flex flex-col gap-4 md:gap-8">
+			<div>
+				<h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+					{aboutUsContent.title}
+				</h1>
+				<p className="leading-7 [&:not(:first-child)]:mt-6">
+					{aboutUsContent.headline}
+				</p>
+			</div>
+
+			<div>
+				<h2 className="mt-8 text-2xl font-semibold tracking-tight">
+					{aboutUsContent.goal.title}
+				</h2>
+				<p className="leading-7 [&:not(:first-child)]:mt-6">
+					{aboutUsContent.goal.description}
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default AboutUsPage;
---
+++ src/components/AboutUsPage/utility.ts
@@ -0,0 +1,10 @@
+export const aboutUsContent = {
+	title: 'About Us',
+	headline:
+		'JobJourney is dedicated to making job tracking simpler and smarter, enabling professionals to take control of their career journey.',
+	goal: {
+		title: 'Our Goal',
+		description:
+			'At JobJourney, our mission is to empower candidates applying to the jobs worldwide by providing a unified, streamlined platform to organize, share, and collaborate. We envision a future where every candidate, has quick and seamless access to applications history and interview insights, and any information they need to thrive.',
+	},
+};
