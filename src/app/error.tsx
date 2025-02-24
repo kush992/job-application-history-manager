34c715f92a07b15161bc5e39d8dfea779ce26e69
Kush <85061303+kush992@users.noreply.github.com>
2025-02-24 16:24:41 +0100

    feat: added drawer and extended filters

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -6,8 +6,8 @@ import { Button } from '@/components/ui/button';
 
 const Error = ({ error, reset }: { error: Error; reset: () => void }) => {
 	return (
-		<div className="flex flex-col items-center justify-center h-screen">
-			<h1 className="text-4xl font-bold text-darkVioletAccent dark:text-lightVioletAccent">
+		<div className="flex flex-col container items-center justify-center h-screen">
+			<h1 className="text-4xl font-bold text-center text-darkVioletAccent dark:text-lightVioletAccent">
 				500 - Internal Server Error
 			</h1>
 			<p className="mt-4">Sorry, something went wrong.</p>
