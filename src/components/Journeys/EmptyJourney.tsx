24230ad0b724e20ae5ab4144ab35a9d64e61fa2e
Kush <85061303+kush992@users.noreply.github.com>
2025-12-09 23:25:48 +0100

    fix: linting

--- src/components/Journeys/EmptyJourney.tsx
+++ src/components/Journeys/EmptyJourney.tsx
@@ -1,11 +1,11 @@
 import { Plus,RocketIcon } from 'lucide-react';
+import Link from 'next/link';
 import React from 'react';
 
 import { appRoutes } from '@/utils/constants';
 
 import { Button } from '../ui/button';
 import { Card, CardContent,CardDescription, CardHeader, CardTitle } from '../ui/card';
-import Link from 'next/link';
 
 const EmptyJourney = () => {
 	return (
